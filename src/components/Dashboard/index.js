c16e6d690307a27a18b72b490c66783f0a95af3f
J Wang <jiautd@gmail.com>
2020-11-15 18:09:25 -0600

    resolve React Hook “useEffect” is called conditionally
    resolved React Hook “useEffect” is called conditionally. The hook should not be used in loops, conditions, or nested functions. Instead, always use Hooks at the top level of your React function.

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import { Typography, Paper, Avatar, CircularProgress, Button } from '@material-ui/core'
+import React, {useState, useEffect} from 'react'
+import {Typography, Paper, Avatar, CircularProgress, Button } from '@material-ui/core'
 import VerifiedUserOutlined from '@material-ui/icons/VerifiedUserOutlined'
 import withStyles from '@material-ui/core/styles/withStyles'
 import firebase from '../firebase'
-import { withRouter } from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
 
 const styles = theme => ({
-	main: {
+    main: {
 		width: 'auto',
 		display: 'block', // Fix IE 11 issue.
 		marginLeft: theme.spacing.unit * 3,
@@ -30,54 +30,57 @@ const styles = theme => ({
 	},
 	submit: {
 		marginTop: theme.spacing.unit * 3,
-	},
+	}
 })
 
 function Dashboard(props) {
-	const { classes } = props
+    const {classes} = props
 
-	if(!firebase.getCurrentUsername()) {
-		// not logged in
-		alert('Please login first')
-		props.history.replace('/login')
-		return null
-	}
+    const [quote, setQuote] = useState('')
 
-	const [quote, setQuote] = useState('')
+    useEffect(() => {
+        if(firebase.getCurrentUsername()) {
+            firebase.getCurrentUserQuote().then(setQuote)
+        }
+    }, [firebase.getCurrentUsername(), firebase.getCurrentUserQuote()])
 
-	useEffect(() => {
-		firebase.getCurrentUserQuote().then(setQuote)
-	})
+    if(!firebase.getCurrentUsername()) {
+        // Not logged in
+        alert('Please login first')
+        props.history.replace('/login')
+        return null
+    }
 
-	return (
-		<main className={classes.main}>
-			<Paper className={classes.paper}>
-				<Avatar className={classes.avatar}>
-					<VerifiedUserOutlined />
-				</Avatar>
-				<Typography component="h1" variant="h5">
-					Hello { firebase.getCurrentUsername() }
-				</Typography>
-				<Typography component="h1" variant="h5">
-					Your quote: {quote ? `"${quote}"` : <CircularProgress size={20} />}
-				</Typography>
-				<Button
-					type="submit"
-					fullWidth
-					variant="contained"
-					color="secondary"
-					onClick={logout}
-					className={classes.submit}>
-					Logout
-          		</Button>
-			</Paper>
-		</main>
-	)
+    return (
+        <main className={classes.main}>
+            <Paper className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <VerifiedUserOutlined />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Hello {firebase.getCurrentUsername()}
+                </Typography>
+                <Typography component="h1" variant="h5">
+                    Your quote: {quote ? `"$quote"` : <CircularProgress size={20} />}
+                </Typography>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="secondary"
+                    onClick={logout}
+                    className={classes.submit}>
+                        Logout
+                    </Button>
+            </Paper>
+        </main>
+    )
 
-	async function logout() {
-		await firebase.logout()
-		props.history.push('/')
-	}
+
+    async function logout() {
+        await firebase.logout()
+        props.history.push('/')
+    }
 }
 
 export default withRouter(withStyles(styles)(Dashboard))
